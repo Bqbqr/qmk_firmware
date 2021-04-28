a2645a64a7253bec88c7d5086c879a534d61e200
Bqbqr <flo@tartampion.com>
2021-04-28 15:28:04 +0200

    testing changes to tapping term
    it is way better for me!

--- keyboards/crkbd/keymaps/bépo_bqbqr/config.h
+++ keyboards/crkbd/keymaps/bépo_bqbqr/config.h
@@ -25,7 +25,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Select hand configuration */
 
 #define MASTER_LEFT
-// #define MASTER_RIGHT
+//#define MASTER_RIGHT
 // #define EE_HANDS
 
 // #define SSD1306OLED
@@ -35,10 +35,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL_PD2
 
 // #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 200
+#define TAPPING_TERM 140
 //#define RETRO_TAPPING
 //#define PERMISSIVE_HOLD
-#define IGNORE_MOD_TAP_INTERRUPT
+//#define IGNORE_MOD_TAP_INTERRUPT
 
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLED_NUM
