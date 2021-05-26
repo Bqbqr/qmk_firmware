81e2e01a2d36ef4d8c99cba411e5ed1f8fc5c478
Bqbqr <flo@tartampion.com>
2021-05-26 17:33:46 +0200

    fix typo in crkbd keymap and create a clean keymap for reviung41

--- keyboards/crkbd/keymaps/bépo_bqbqr/keymap.c
+++ keyboards/crkbd/keymaps/bépo_bqbqr/keymap.c
@@ -71,8 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RAISE] = LAYOUT(
   //,-----------------------------------------.                ,-----------------------------------------.
-     KC_ESC,KC_EXLM,KC_AT,KC_HASH,KC_DLR,KC_PERC,              
-      ,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_BSPC,
+     KC_ESC,KC_EXLM,KC_AT,KC_HASH,KC_DLR,KC_PERC,              KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_BSPC,
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
     KC_LSFT, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                KC_MINS,KC_EQL,KC_LCBR,KC_RCBR,KC_PIPE,KC_GRV,
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
