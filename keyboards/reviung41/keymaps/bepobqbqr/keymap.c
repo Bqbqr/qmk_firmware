1979101f798934239a30fd2cd46d1562171ac26b
Bqbqr <flo@tartampion.com>
2021-06-14 09:51:05 +0200

    Add bastardkb files and config.h for reviung to deal with led.

--- keyboards/reviung41/keymaps/bepobqbqr/keymap.c
+++ keyboards/reviung41/keymaps/bepobqbqr/keymap.c
@@ -14,6 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+#include QMK_KEYBOARD_H
 #include "keymap_bepo.h"
 
 
@@ -30,7 +31,7 @@ enum layer_names {
 #define LOWER  MO(_LOWER)
 #define RAISE  MO(_RAISE)
 #define ADJUST MO(_ADJUST)
-#define KC_RACL LALT_T(KC_ESC)
+#define KC_RACL RALT_T(KC_ESC)
 #define KC_SFTAB LSFT_T(KC_TAB)
 
 
@@ -73,9 +74,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   
   [_ADJUST] = LAYOUT_reviung41(
 //,-----------------------------------------.                ,-----------------------------------------.
-      RESET,KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                  KC_MPRV,KC_MPLY, KC_MNXT, KC_NO, KC_NO, KC_NO,
+      RESET,RGB_M_R, KC_NO, KC_NO, KC_NO, KC_NO,                  KC_MPRV,KC_MPLY, KC_MNXT, KC_NO, KC_NO, KC_NO,
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-    KC_NO,KC_NO,KC_NO,KC_NO,KC_PSCREEN,KC_NO,               KC_AUDIO_MUTE,KC_VOLU, KC_NO, KC_NO, KC_NO, KC_NO,
+    RGB_TOG,KC_NO,KC_NO,KC_NO,KC_PSCREEN,KC_NO,               KC_AUDIO_MUTE,KC_VOLU, KC_NO, KC_NO, KC_NO, KC_NO,
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
     TG(6),KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,               KC_SCROLLLOCK,KC_VOLD, KC_NO, KC_NO, KC_NO, RGB_RMOD,
   //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
@@ -122,3 +123,41 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
+
+
+
+// Here start the magic to light up shit
+#ifdef RGBLIGHT_ENABLE
+// Light LEDs 6 to 9 and 12 to 15 red when caps lock is active. Hard to ignore!
+const rgblight_segment_t PROGMEM numberlayer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {1, 10, HSV_BLUE}
+);
+
+const rgblight_segment_t PROGMEM lowerlayer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {1, 10, HSV_GREEN}
+);
+
+// Now define the array of layers. Later layers take precedence
+const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+    numberlayer,
+    lowerlayer
+); 
+
+void keyboard_post_init_user(void) {
+    // Enable the LED layers
+    rgblight_layers = my_rgb_layers;
+    rgblight_enable_noeeprom(); // Enables RGB, without saving settings
+    rgblight_sethsv_noeeprom(HSV_PURPLE);
+    rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+}
+
+
+layer_state_t default_layer_state_set_user(layer_state_t state) {
+    rgblight_set_layer_state(0, IS_LAYER_ON_STATE(state, 1));
+    rgblight_set_layer_state(1, IS_LAYER_ON_STATE(state, 4));
+    return state;
+}
+
+
+
+#endif
